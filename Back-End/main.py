118ebb9a18a0e74de107eca85e69115084df26a0
MamadAminMousavi <mamadamin.mousavi86@gmail.com>
2024-04-09 20:03:53 +0330

    Create CRUD Categories

--- Back-End/main.py
+++ Back-End/main.py
@@ -133,6 +133,98 @@ def delete_user_by_id(user_id):
 #-------------------------------------------------
 
 
+#category crud function
+def get_all_Categories():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Categories')
+    Categories = cur.fetchall()
+    final_Categories = []
+    for Categorie in Categories:
+        final_Categories.append({
+            "category_id": Categorie[0],
+            "name": Categorie[1],
+            "description": Categorie[2],
+            "parent_category_id": Categorie[3],
+            "created_at": Categorie[4],
+        })
+    conn.close()
+    return final_Categories
+def create_Categories(name, description, parent_category_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    created_at = datetime.today().strftime('%Y-%m-%d')
+    cur.execute('INSERT INTO Categories (name, description,parent_category_id,created_at) VALUES (?, ?, ?, ?)', (name, description, parent_category_id, created_at))
+    conn.commit()
+    conn.close()
+    return "ok"
+def get_Categories(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Categories WHERE category_id = ?',(id,))
+    Categorie = cur.fetchone()
+    conn.close()
+    final_category = {
+        "category_id": Categorie[0],
+        "name": Categorie[1],
+        "description": Categorie[2],
+        "parent_category_id": Categorie[3],
+        "created_at": Categorie[4],
+    }
+    return final_category
+def delete_category(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM Categories WHERE category_id = ?', (id,))
+    conn.commit()
+    conn.close()
+def update_category(name,description,parent_category_id,created_at,id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('UPDATE Categories SET name = ?, description = ?, parent_category_id = ?, created_at = ? WHERE category_id = ?', (name,description,parent_category_id,created_at,id))
+    conn.commit()
+    conn.close()
+    return get_Categories(id)
+
+
+# category crud routes
+@app.route('/Categories', methods=['GET'])
+def list_Categories():
+    Categories = get_all_Categories()
+    response = jsonify(Categories)
+    response.headers['Access-Control-Expose-Headers'] = 'Content-Range'
+    response.headers['Content-Range'] = len(Categories)
+    return response
+@app.route('/Category/<int:id>', methods=['GET'])
+def Category(id):
+    Category = get_Categories(id)
+    if Category is None:
+        return '', 404
+    return jsonify(Category), 200
+@app.route('/Categories', methods=['POST'])
+def add_Categories():
+    name = request.json['name']
+    description = request.json['description']
+    parent_category_id = request.json['parent_category_id']
+    Categories_id = create_Categories(name, description, parent_category_id)
+    return "ok", 201
+@app.route('/Category/<int:id>', methods=['DELETE'])
+def delete_category_by_id(id):
+    delete_category(id)
+    return jsonify({"id":id}), 200
+@app.route('/Category/<int:id>', methods=['PUT'])
+def update_category_by_id(id):
+    name = request.json['name']
+    description = request.json['description']
+    parent_category_id = request.json['parent_category_id']
+    created_at = request.json['created_at']
+    updated = update_category(name, description, parent_category_id, created_at,id)
+    return jsonify(updated), 200
+
+
+#-------------------------------------------------
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
